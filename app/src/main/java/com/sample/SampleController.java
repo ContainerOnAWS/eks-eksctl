44f82804258213e19ab14c055c01ae258b5d7708
engel80 <prettyhip@gmail.com>
2022-08-15 10:43:59 +0900

    .

---
+++ app/src/main/java/com/sample/SampleController.java
@@ -0,0 +1,29 @@
+package com.sample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String home() {
+        return new String("OK");
+    }
+
+    @RequestMapping(value="/{seviceid}/{pathvar1}", method=RequestMethod.GET)
+    public String pingPath2() {
+        return new String("OK");
+    }
+
+    @RequestMapping(value="/{seviceid}/{pathvar1}/{pathvar2}", method=RequestMethod.GET)
+    public String pingPath3() {
+        return new String("OK");
+    }
+
+    @RequestMapping(value="/{seviceid}/{pathvar1}/{pathvar2}/{pathvar3}", method=RequestMethod.GET)
+    public String pingPath4() {
+        return new String("OK");
+    }
+}
